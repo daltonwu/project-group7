2c9b7fe42abdcd5a8faa7daa637c9561babaa46d
daltonwu <dalton.z.wu@gmail.com>
2014-10-15 17:28:33 -0400

    Update Basechar.java

--- Basechar.java
+++ Basechar.java
@@ -1,19 +1,45 @@
 public class Basechar {
-    private int hp; // health points
-    private int ep; // energy points
+    // name of the thing
+    private String name;
     
-    private int str;
+    // health and energy
+    private int hp, ep;
     
-    public Basechar(int hp int ep {
-	this.hp = hp;
-	this.ep = ep;
+    // attributes 'n' stuff
+    private int str, wis, spd, dex;
+    
+    
+    // constructors
+    public Basechar(String name, int hp, int ep) {
+        setName(name);
+        setHP(hp);
+        setEP(ep);
     }
     public Basechar() {
     	hp = 20;
     	ep = 0;
     }
     
+    
+    // setters and getters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return this.name;
+    }
+    
+    public void setHP(int hp) {
+    	this.hp = hp;
+    }
     public int getHP() {
     	return hp;
+    }
+    
+    public void setEP(int ep) {
+    	this.ep = ep;
+    }
+    public int getEP() {
+    	return ep;
     }
 }
