cedcac5f963698db2f20a2884cb50d6294373ed6
daltonwu <dalton.z.wu@gmail.com>
2014-10-15 14:45:48 -0400

    Update Basechar.java

--- Basechar.java
+++ Basechar.java
@@ -5,7 +5,15 @@ public class Basechar {
     private int str;
     
     public Basechar(int hp int ep {
-	this.hp
-	this.ep
+	this.hp = hp;
+	this.ep = ep;
+    }
+    public Basechar() {
+    	hp = 20;
+    	ep = 0;
+    }
+    
+    public int getHP() {
+    	return hp;
     }
 }
