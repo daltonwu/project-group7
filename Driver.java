252af5440064c2ea2aaafa5535babd96f85ed8e6
Dalton Wu <dalton.z.wu@gmail.com>
2014-10-17 09:27:47 -0400

    YEAH

--- Driver.java
+++ Driver.java
@@ -1,6 +1,24 @@
-import java.util.*; // includes io and random
+import java.util.io.*;
+import java.util.Random;
 
 public class Driver {
-    Random rand = new Random();
-    Scanner input = new Scanner(System.in);
+    static Random rand = new Random();
+    static Scanner input = new Scanner(System.in);
+    
+    
+    
+    public static void main(String[] args) {
+	init();
+    }
+    
+    public static void init() {
+	// initialize the game
+	System.out.println("generic welcome");
+	System.out.println("pick yo' char\n[N] fo' ninja, [C] fo' cowboy");
+	
+	
+    }
+    
+    public static boolean isWin() {
+    }
 }
