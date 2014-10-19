527195ab267589621be7b7216f1eb30fef0fcbe5
Pranab <Islam>
2014-10-19 14:04:42 -0400

    changed operation signs

--- Cowboy.java
+++ Cowboy.java
@@ -17,22 +17,22 @@ public class Cowboy extends Basechar {
 
     public int shot(){
 	dmg = 10;
-	ammo = ammo - 1;
-	ep = ep - 5
+	ammo -= 1;
+	ep -= 5
 	return dmg;
     }
 
     public int doubleShot(){
-	ammo = ammo - 2;
+	ammo -= 2;
 	dmg = 20;
-	ep = ep -10;
+	ep -= 10;
 	return dmg;
     }
 
     public int bangBangAttack(){
 	ammo = 0;
 	dmg = 50;
-	ep = ep - 24;
+	ep -= 24;
 	return dmg;
 
     }
