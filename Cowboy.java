7cee8ca1908d2b10233bcfe5fbee08a8ad40b51d
Pranab <Islam>
2014-10-19 12:56:37 -0400

    added the basics

--- Cowboy.java
+++ Cowboy.java
@@ -1,2 +1,22 @@
-public class Cowboy {
+public class Cowboy extends Basechar {
+
+    private int ammo;
+    
+    public Cowboy(String name, int hp, int ep, int dex, int spd, int str, int wis){
+    super(name, 20, 25, 15, 15, 40, 25);
+    }
+    
+    public void setAmmo(){
+    ammo = 6;
+    }
+
+    public void getEnergy(){
+	
+    }
+
+    public String attack(Basechar other){
+    return name + "attacked that darn ninja vermon";
+    }
+
+
 }
