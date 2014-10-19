577b0c784ef0f081d70e44238fb158ff1d5d6831
Remi Moon <remim00n7@gmail.com>
2014-10-19 14:18:12 -0400

    sjdnfdjf

--- Ninja.java
+++ Ninja.java
@@ -2,13 +2,27 @@ public class Ninja extends Basechar {
     public Ninja(String name, int hp, int ep, int dex, int spd, int str, int wis) {
 	super (name, 20, 30, 30, 30, 30, 10, 10);
     }
+    //im just gunna call the ninja weapon thing stars
+    private int stars;
+    private int dmg;
 
-    public void attack (Basechar other) {
+    public void setStars(){
+	stars = 10;
+    }
+
+    /*   public void attack (Basechar other) {
 	System.out.println(this + " is attacking " + other);
     }
 
     public void defend (Basechar other){
 	System.out.println(this + " is defending against " + other);
+    }
+    */
+    public int throwStars(){
+	dmg = 5;
+	stars-=;
+	ep-= 5;
+	return dmg;
     }
 
 
