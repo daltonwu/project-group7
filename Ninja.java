c31ef708a5dcc37ab8018fbb0f3f5b5636e2d91e
Remi Moon <remim00n7@gmail.com>
2014-10-19 14:28:03 -0400

    updating ninja, added attacks

--- Ninja.java
+++ Ninja.java
@@ -2,28 +2,36 @@ public class Ninja extends Basechar {
     public Ninja(String name, int hp, int ep, int dex, int spd, int str, int wis) {
 	super (name, 20, 30, 30, 30, 30, 10, 10);
     }
+
     //im just gunna call the ninja weapon thing stars
     private int stars;
+   
     private int dmg;
 
     public void setStars(){
 	stars = 10;
     }
 
-    /*   public void attack (Basechar other) {
-	System.out.println(this + " is attacking " + other);
-    }
-
-    public void defend (Basechar other){
-	System.out.println(this + " is defending against " + other);
-    }
-    */
     public int throwStars(){
 	dmg = 5;
-	stars-=;
-	ep-= 5;
+	stars -= 1;
+	ep -= 5;
 	return dmg;
     }
+
+    public int superKick(){
+	dmg = 10;
+	ep -= 7;
+	return dmg;
+    }
+
+    public int starCombo(){
+	dmg = 15;
+	ep -= 10;
+	stars -= 5;
+	return dmg;
+    }
+
 
 
 
