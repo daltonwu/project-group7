703b03ccbc0641e9dc2ed7d6a07f2f19ebffc891
daltonwu <dalton.z.wu@gmail.com>
2014-10-21 01:22:42 -0400

    updated syntax / semantics

--- Cowboy.java
+++ Cowboy.java
@@ -1,63 +1,48 @@
 public class Cowboy extends Basechar {
-
+    // Cowboy-specific variables
     private int ammo;
-    private int dmg;
-    private int ep;
-
-    public Cowboy(){
-	super();
+    
+    
+    // constructors
+    public Cowboy() {
+	    super();
     }
-
-    public Cowboy(String name){
-    super(name, 20, 25, 15, 15, 40, 25);
+    public Cowboy(String name) {
+        super(name, 20, 25, 15, 15, 40, 25);
     }
     
-    public void setAmmo(){
-    ammo = 6;
+    
+    // setters 'n' getters
+    public void setAmmo() {
+        this.ammo = 6;
     }
-
-    public void setEP(){
-	int ep = super.getEP();
-    }
-
-    public void getEnergy(){
-	
-    }
-
-    public int shot(){
-	if (ep >= 5){
-	dmg = 10;
-	ammo -= 1;
-	ep -= 5
-	return dmg;
-	}
-	else {
+    
+    
+    // ATTACKS
+    public int shot() {
+    	if(this.getEP() >= 5) {
+        	this.ammo -= 1;
+        	this.addEP(5);
+        	return 10;
+    	}
 	    return -1;
-	}
     }
-
-    public int doubleShot(){
-	if (ep >= 10){
-	ammo -= 2;
-	dmg = 20;
-	ep -= 10;
-	return dmg;
-	}
-	else {
-	    return -1;
-	}
+    
+    public int doubleShot() {
+    	if(this.getEP() >= 10) {
+        	this.ammo -= 2;
+        	this.addEP(-10);
+        	return 20;
+    	}
+        return -1;
     }
-
-    public int bangBangAttack(){
-	if (ep >= 24){
-	ammo = 0;
-	dmg = 50;
-	ep -= 24;
-	return dmg;
-	}
-	else {
-	    return -1;
-	}
-
+    
+    public int bangBangAttack() {
+    	if (this.getEP() >= 24) {
+        	this.ammo = 0;
+        	this.addEP(-24);
+        	return 50;
+    	}
+        return -1;
     }
 }
