5fded9eb35e93caeec49e4ae0da902ef503b8907
TiffL <lei.tiffany@gmail.com>
2014-10-20 23:25:48 -0400

    Edits 10-20-14

--- Driver.java
+++ Driver.java
@@ -3,13 +3,11 @@ import java.io.*;
 
 public class Driver {
     public static void main(String[] args){
-	String s = "";
 	String PlayerChar = new Driver().AskUser("Choose a character([c] for Cowboy or [n] for  Ninja?): ");
         String name = new Driver().AskUser("Choose your character's name: ");
 	
 	Cowboy c;
 	Ninja n;
-	PlayerChar = PlayerChar.toLowerCase();
 	
 	if (PlayerChar.equals("c")){
 	    c = new Cowboy(name);
@@ -19,14 +17,13 @@ public class Driver {
 	    n = new Ninja(name);
 	    c = new Cowboy();
 	}
-	//System.out.println("Your Character's name is: " + c.getName());
+	// System.out.println("Your Character's name is: " + c.getName());
 
-        String log = "";
 	while (c.getHP()>0 && n.getHP()>0){
-	    log = log + new ContinueEncounter(PlayerChar,c,n,log);
+	    new ContinueEncounter(PlayerChar,c,n);
+	    new ComputerResponse(PlayerChar,c,n);
 	}
 
-	//s = "done";
 	if ((n.getHP() == 0 && PlayerChar.equals("c"))||
 	    (c.getHP() == 0 && PlayerChar.equals("n"))){
 	    System.out.println("Congratulations! You've won!");
@@ -37,40 +34,76 @@ public class Driver {
     }
 
     public String AskUser(String mToUser){
-	String s = "";
-	Scanner sc = new Scanner(System.in);
-	System.out.print(mToUser);
-	s = sc.nextLine();
-	return s;
+    	String s = "";
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print(mToUser);
+    	s = sc.nextLine();
+    	return s.toLowerCase();
     }
 
-    public String ContinueEncounter(String PlayerChar, Cowboy x, Ninja y, String log){
-    	String s = "";
-    	s = AskUser("Attack or Defend?");
+    public String ContinueEncounter(String PlayerChar, Cowboy x, Ninja y){
+    	String s = AskUser("Attack or Defend?"); // only attack for now
     	if (PlayerChar.equals("c")){
-    	    if (s.equals("Attack") && c.getEP()>5){
-		r = AskUser("Shoot, DoubleShoot, or ___?");
-		r = r.toLowerCase();
-    		if (r.equals("shoot")){c.shot();}
-		else if(r.equals("doubleshoot")){c.doubleShot();}
-		else if(r.equals("___")){c.bangBangAttack();}
+	    int ep = c.getEP();
+    	    if (s.equals("attack")){
+		if (ep == 0){
+		    r = AskUser("[1]LickWounds,[2]Chill");
+		    if (r.equals("1")){c.lickWounds()}
+		    else {c.chill()}
+		}
+		else{
+		    int i = 0;
+		    while (i != -1){	
+			r = AskUser("[1]Shoot, [2]DoubleShoot, or [3]BangAttack?");
+			if (r.equals("1") && ep-5!=0){c.shot(); i=1}
+			else if(r.equals("2") && ep-10!=0){c.doubleShot(); i=1}
+			else if(r.equals("3") && ep-24!=0){c.bangBangAttack(); i=1}
+			else{i=-1}
+		    }
+		}
     	    }
-    	    else if (s.equals("Defend")){
-    		c.defend();
-    	    }
+    	    // else if (s.equals("Defend")){
+    	    // 	c.defend();
+    	    // }
     	}
         else{
-    	    if (s.equals("Attack") && n.getEP()>5){
-		r = AskUser("ThrowStars, SuperKick, or StarCombo?");
-		r = r.toLowerCase();
-		if (r.equals("throwstars")){n.throwStars();} 
-		else if(r.equals("superkick")){n.superKick();}
-		else if(r.equals("starcombo")){n.starCombo();}
-	    }
-	    else if (s.equals("Defend")){
-    		n.defend();
+	    int ep = n.getEP();
+    	    if (s.equals("attack")){
+		if (ep == 0){
+		    r = AskUser("[1]LickWounds,[2]Chill");
+		    if (r.equals("1")){n.lickWounds()}
+		    else {n.chill()}
+		else{
+		    int i = 0;
+		    while (i != -1){
+			r = AskUser("[1]ThrowStars, [2]SuperKick, [3]StarCombo, or [4]Katana?");
+			if (r.equals("1")&& ep-5!=0){n.throwStars(); i=1} 
+			else if(r.equals("2"&& ep-7!=0)){n.superKick(); i=1}
+			else if(r.equals("3"&& ep-10!=0)){n.starCombo(); i=1}
+			else if(r.equals("4"&& ep-1!=0)){n.katanaHit(); i=1}
+			else{i=-1;}
+		    }
+		}  
     	    }
+    	    // else if (s.equals("Defend")){
+    	    // 	n.defend();
+    	    // }
     	}
     }	
-	
+    
+    public String ComputerResponse(String PlayerChar, Cowboy x, Ninja y){
+    	Random r = new Random();
+    	int d = r.nextInt(4);
+    	if (PlayerChar.equals("c")){
+    	    if (d.equals("0")){n.throwStars();} 
+    	    else if(d.equals("1")){n.superKick();}
+    	    else if(d.equals("2")){n.starCombo();}
+    	    else {n.katanaHit();}
+    	}
+    	else{
+    	    if (d.equals("0")){c.shot();}
+    	    else if(d.equals("1")){c.doubleShot();}
+    	    else {c.bangBangAttack();}
+    	}
+    }
 }
